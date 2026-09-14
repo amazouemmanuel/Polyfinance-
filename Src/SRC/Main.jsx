9ad57d9288a4c349549b4df7e06263532bbb945f
amazouemmanuel <amazouemmanuel274@gmail.com>
2026-09-14 02:00:15 +0000

    Update Main.jsx

--- Src/SRC/Main.jsx
+++ Src/SRC/Main.jsx
@@ -10,3 +10,8 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => navigator.serviceWorker.register("/sw.js"));
+}
+

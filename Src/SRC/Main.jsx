fe8fd11d0e08bb7a90e2f5daba3fa192d6af97f7
amazouemmanuel <amazouemmanuel274@gmail.com>
2026-09-19 10:46:07 +0000

    Update Main.jsx

--- Src/SRC/Main.jsx
+++ Src/SRC/Main.jsx
@@ -1,7 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { inject } from "@vercel/analytics";
+import * as Sentry from "@sentry/react";
 import App from "./App.jsx";
+
+Sentry.init({
+  dsn: "https://d39e25f7a06e8997d9fe74e8c4ffccd0@o4512109832175616.ingest.de.sentry.io/4512110033829969",
+});
 
 inject();
 
@@ -14,4 +19,5 @@ createRoot(document.getElementById("root")).render(
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => navigator.serviceWorker.register("/sw.js"));
 }
+
 
